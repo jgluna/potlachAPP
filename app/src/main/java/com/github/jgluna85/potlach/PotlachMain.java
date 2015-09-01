ee3102f448fce43ff9d1e9e610854654783d5b09
Juan Luna <jgluna85@gmail.com>
2015-08-31 22:04:14 -0500

    added initial activity

--- app/src/main/java/com/github/jgluna85/potlach/PotlachMain.java
+++ app/src/main/java/com/github/jgluna85/potlach/PotlachMain.java
@@ -11,20 +11,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 public class PotlachMain extends AppCompatActivity {
 
-    private ImageView cameraView;
-    private ImageView folderView;
+    private ImageView loadedImage;
+    private EditText imageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_potlach_main);
-        cameraView = (ImageView) findViewById(R.id.takePhoto);
-        cameraView.setOnClickListener(new View.OnClickListener() {
+        loadedImage = (ImageView) findViewById(R.id.selectedImage);
+        imageText = (EditText) findViewById(R.id.titleField);
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.photoButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -34,8 +39,8 @@ public class PotlachMain extends AppCompatActivity {
 
             }
         });
-        folderView = (ImageView) findViewById(R.id.selectFromFile);
-        folderView.setOnClickListener(new View.OnClickListener() {
+        ImageButton folderButton = (ImageButton) findViewById(R.id.folderButton);
+        folderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(
@@ -56,31 +61,35 @@ public class PotlachMain extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            cameraView.setImageBitmap(imageBitmap);
-        } else if (requestCode == 2 && resultCode == RESULT_OK) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                loadedImage.setImageBitmap(imageBitmap);
+                loadedImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                cursor.moveToFirst();
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                cursor.close();
 
-            folderView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                loadedImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                loadedImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            }
+            imageText.setVisibility(View.VISIBLE);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) loadedImage.getLayoutParams();
+            params.weight = 90;
+            findViewById(R.id.buttonsLayout).setVisibility(View.GONE);
         }
     }
 }
